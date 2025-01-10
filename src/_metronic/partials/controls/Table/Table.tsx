089d73205ef3b930e8e4096c0db34ef95bc10a80
Mehrdad Imani <Abolfazl.Imani@outlook.com>
2025-01-10 15:43:36 +0330

    update table

--- src/_metronic/partials/controls/Table/Table.tsx
+++ src/_metronic/partials/controls/Table/Table.tsx
@@ -1,78 +1,126 @@
-import { useEffect, useMemo, useState } from 'react';
-import { useTable, ColumnInstance, Row, Column } from 'react-table';
+import {
+  Column,
+  ColumnDef,
+  ColumnFiltersState,
+  RowData,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from '@tanstack/react-table'
 import { useTranslation } from 'react-i18next';
-import { CustomHeaderColumn } from './components/columns/CustomHeaderColumn';
-import { CustomRow } from './components/columns/CustomRow';
-import { ListPagination } from './components/pagination/ListPagination';
-import { ListLoading } from './components/loading/ListLoading';
-import { useQueryResponse, useQueryResponseLoading } from './core/QueryResponseProvider';
-import '../styles/Table2.css';
-import { useListView } from './core/ListViewProvider';
+import { CustomHeaderColumn } from './components/columns/CustomHeaderColumn'; // Import your custom header column component
+import { CustomRow } from './components/columns/CustomRow'; // Import your custom row component
+import { ListPagination } from './components/pagination/ListPagination'; // Import your pagination component (optional)
+import { ListLoading } from './components/loading/ListLoading'; // Import your loading component (optional)
+import { useQueryResponse, useQueryResponseLoading } from './core/QueryResponseProvider'; // Assuming this provides data and loading state
+import { useListView } from './core/ListViewProvider'; // Assuming this provides selection state (optional)
+import '../styles/Table2.css'; // Import your styles (optional)
+import { useEffect, useState } from 'react';
 import { Card } from 'react-bootstrap';
 
-export const Table = ({ columns }: { columns: ReadonlyArray<Column<any>> }) => {
+export const Table = ({ columns }: { columns: ColumnDef<any>[] }) => {
   const { t } = useTranslation();
-  const reponseContext = useQueryResponse();
+  const responseContext = useQueryResponse();
   const isLoading = useQueryResponseLoading();
-  const [data, setData] = useState(reponseContext.response?.Data?.Result || []);
-  const cols = useMemo(() => columns, []);
+  const [data, setData] = useState(responseContext?.response?.Data?.Result || []);
   const listView = useListView();
 
   useEffect(() => {
-    setData(reponseContext.response?.Data?.Result || []);
-  }, [reponseContext]);
+    setData(responseContext?.response?.Data?.Result || []);
+  }, [responseContext]);
 
-  const { getTableProps, getTableBodyProps, headers, rows, prepareRow } = useTable({
-    columns: cols,
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>(
+    []
+  )
+
+  const table = useReactTable({
     data,
-  });
+    columns,
+    filterFns: {},
+    state: {
+      columnFilters,
+    },
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(), //client side filtering
+    getSortedRowModel: getSortedRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    debugTable: true,
+    debugHeaders: true,
+    debugColumns: false,
+  })
 
   return (
     <Card>
       <Card.Body className='p-5'>
-        <div className='table-responsive'>
-          <table
-            id='kt_table_users'
-            className='table border-light-subtle align-middle table-row-dashed fs-6 gy-3 dataTable no-footer'
-            {...getTableProps()}
-          >
+      <div className='table-responsive'>
+      <table className='table border-light-subtle align-middle table-row-dashed fs-6 gy-3 dataTable no-footer'>
             <thead>
-              <tr className='text-start text-muted fw-bolder fs-7 text-uppercase gs-0'>
-                {listView?.isSelectable && (
-                  <th className='ps-2'>
-                    <input type='checkbox' title='انتخاب همه' checked={listView.isAllSelected} onChange={(e) => {
-                    if (e.target.checked)
-                      listView.onSelectAll();
-                    else
-                      listView.clearSelected();
-                  }} /></th>
-                )}
-                {headers.map((column: ColumnInstance<object>) => (
-                  <CustomHeaderColumn key={column.id} column={column} />
-                ))}
-              </tr>
-            </thead>
-            <tbody className='text-gray-600 fw-bold' {...getTableBodyProps()}>
-              {rows.length > 0 ? (
-                rows.map((row: Row<object & {Id: number}>, i) => {
-                  prepareRow(row);
-                  return <CustomRow row={row} key={`row-${i}-${row.id}`} />;
-                })
-              ) : (
-                <tr>
-                  <td colSpan={cols.length}>
-                    <div className='d-flex text-center w-100 align-content-center justify-content-center'>
-                      {t('Messages.NoRecords')}
-                    </div>
-                  </td>
+              {table.getHeaderGroups().map(headerGroup => (
+                <tr key={headerGroup.id}>
+                  {headerGroup.headers.map(header => {
+                    return (
+                      <th key={header.id} colSpan={header.colSpan}>
+                        {header.isPlaceholder ? null : (
+                          <>
+                            <div
+                              {...{
+                                className: header.column.getCanSort()
+                                  ? 'cursor-pointer select-none'
+                                  : '',
+                                onClick: header.column.getToggleSortingHandler(),
+                              }}
+                            >
+                              {flexRender(
+                                header.column.columnDef.header,
+                                header.getContext()
+                              )}
+                              {{
+                                asc: ' 🔼',
+                                desc: ' 🔽',
+                              }[header.column.getIsSorted() as string] ?? null}
+                            </div>
+                            {header.column.getCanFilter() ? (
+                              <div>
+                                {/* <Filter column={header.column} /> */}
+                              </div>
+                            ) : null}
+                          </>
+                        )}
+                      </th>
+                    )
+                  })}
                 </tr>
-              )}
+              ))}
+            </thead>
+            <tbody>
+              {table.getRowModel().rows.map(row => {
+                return (
+                  <tr key={row.id}>
+                    {row.getVisibleCells().map(cell => {
+                      return (
+                        <td key={cell.id}>
+                          {flexRender(
+                            cell.column.columnDef.cell,
+                            cell.getContext()
+                          )}
+                        </td>
+                      )
+                    })}
+                  </tr>
+                )
+              })}
             </tbody>
           </table>
         </div>
-        <ListPagination />
         {isLoading && <ListLoading />}
       </Card.Body>
+        <Card.Footer className='p-1 pt-0'>
+        <ListPagination />
+        </Card.Footer>
     </Card>
   );
 };
